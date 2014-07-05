da5b96403171fbf709337a53d96a2c1e827c00e4
Josh Daniels <daniels.joshua100@gmail.com>
2014-07-05 19:30:19 -0400

    got audio starting,buttons working and damage is being dealt and returned

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,22 @@ $(".startbutton").click(function(){
 	$(".titlescreen").show();
 	$(".logogif").show();
 });
+
+
+
+
+  $(".startbutton").click(function(){
+  document.getElementById('audio1').play();
+  });
+  	
+$(".clicktostart").click(function(){
+	$(".road").show();
+	
+});
+
+
+$(".attackbutton").click(function(){
+	$(".yellowcardamage").show().delay(2000).fadeOut(400,function(){
+                       $(".blackcardamage").show().delay(2000).fadeOut();
+});
+});
